e0766b2fb8c3569182a2b017cbfcd6aca5cfacad
Yunong Liu <yl2494@cornell.edu>
2015-01-21 22:36:26 -0500

    kinda like DP, a hard one

--- CC150/Mathematics&probability/findKthNumber.java
+++ CC150/Mathematics&probability/findKthNumber.java
@@ -1,3 +1,11 @@
 /** 7.7 Design an alg to find the kth number such that the only prime factors are 3, 5 and 7.
  *  
  */
+ 
+//9:45pm
+
+/** I think:
+ *  1 |3 5 7| 3*3 3*5 3*7| 5*5 3*3*3
+ */
+ 
+//initial
